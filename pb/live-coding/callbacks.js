bbd1e93c56300a201719a6d38e2697d885886872
gnuffer <gfnuffer@gmail.com>
2021-09-27 10:54:56 +0200

    Add minor edits

--- pb/live-coding/callbacks.js
+++ pb/live-coding/callbacks.js
@@ -4,23 +4,26 @@
 // In JavaScript, functions are "first-class citizens".  
 // Functions are not only used to manipulate values like strings, numbers, booleans, but
 // they are themselves values. 
-// Functions are "first-class objects", that is they are
+// Functions are "first-class objects", that is, they can be ...
+
 // 1. created via literals
 function myFunc() {
     // do something
 }
-// 2. can be the values of variables
+
+// 2. the values of variables
 myFunc = () => { // do something 
 };
-// 3, can be the elements of arrays
-// 4. can be the properties of objects (methods)
-// 5. can be the arguments of other functions
-// 6. can be the return values of other functions
+
+// 3. the elements of arrays
+// 4. the properties of objects (methods)
+// 5. the arguments of other functions
+// 6. the return values of other functions
 
 // a function that is passed as an argument to a function is called a "callback function";
 // a function that takes a function argument is a higher-order function
 
-// A very simple (useless) example of a callback function
+// A very simple (and useless) example of a callback function
 let wrapper = function (callback) {
     return callback();
 }
@@ -38,7 +41,7 @@ const add = function (x, y) {
 };
 console.log(wrapper(2, 3, add));
  
-// the idea of a callback as a function that is "called back" when some action is completed
+// the idea of a callback: a function that is "called back" when some action is completed
 // for example, you might request some resource from a server and call some function (the callback)
 // when the response arrives
 function requestResource(res, cb) {
@@ -50,8 +53,9 @@ function requestResource(res, cb) {
 function cb1() {
     console.log('A');
 }
-// setTimeout is called an "asynchronous" function: it doesn't pause/block execution of subsequent
+// setTimeout is called an "asynchronous" function: it doesn't pause/block the execution of subsequent
 // code but returns immediately.  Immediately after setTimeout is called, console.log is called.
+// "A" prints after "B"
 setTimeout(cb1, 3000);
 console.log('B');
 
@@ -61,4 +65,3 @@ function print(val) {
 // synchronous higher-order function; waits until callback is done.
 [1, 2, 3].forEach(print);
 console.log(11);
-
