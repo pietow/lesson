e413b7e9e87c6f651dc7b25e830a1f54565033ff
gnuffer <gfnuffer@gmail.com>
2021-10-31 20:32:54 +0100

    spa: add events live code

---
+++ spa/live-coding/02-events/events.js
@@ -0,0 +1,41 @@
+const myBtn = document.getElementById('myBtn');
+
+// 3 ways to attach an event handler to an element
+
+// 1. as an HTML attribute (considered bad practice)
+
+// 2. as a property of the element (disadvantage: allows for only one event handler)
+
+myBtn.onclick = function() { // overwritten by next event handler
+    alert('Good afternoon');
+}
+
+myBtn.onclick = function() { // overwrites previous event handler
+    console.log('Good evening');
+}
+
+// 3. with addEventListener (advantage: allows for more than one event handler)
+myBtn.addEventListener('click', function() {
+    alert('Good morning');
+})
+myBtn.addEventListener('click', function() { // no conflict with previous event handler; both will be triggered by click events
+    console.log('Good evening');
+    console.log(this);
+})
+
+// the third argument: capture (true, false)
+document.getElementById('myDiv').addEventListener('click', function() {
+    alert('You clicked on the DIV element');
+}, false)
+
+document.getElementById('myP').addEventListener('click', function() {
+    alert('You clicked on the P element');
+}, false)
+
+document.getElementById('myP2').addEventListener('click', function() {
+    alert('You clicked on the P2 element');
+}, true)
+
+document.getElementById('myDiv2').addEventListener('click', function() {
+    alert('You clicked on the DIV2 element');
+}, true)
