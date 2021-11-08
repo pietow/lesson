a75045e56cd96dc43ee827c13adb075ea8f1149c
gnuffer <gfnuffer@gmail.com>
2021-11-08 13:12:27 +0100

    SPA: add async/await

---
+++ spa/live-coding/07-async-4/index.js
@@ -0,0 +1,40 @@
+// what we can't do because setTimeout is asynchronous
+let greeting = setTimeout(function() {
+    return 'Hi!';
+}, 3000);
+console.log(greeting); // undefined
+
+// what we can do with promises
+let greeting = new Promise((resolve, reject) => {
+    setTimeout(function() {
+        resolve('Hi!');
+    }, 3000);
+});
+greeting.then((result) => { console.log(result) }); //  Hi!
+
+// what we can do with async/await
+async function greetWithDelay() {
+    let greeting = new Promise((resolve, reject) => {
+        setTimeout(function() {
+            resolve('Hello!');
+        }, 3000);
+    });
+    let myGreeting = await greeting;
+    console.log(myGreeting);
+}
+greetWithDelay(); // Hello!
+
+// after refactoring
+function getGreeting() {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            resolve('Ciao!');
+        }, 3000);
+    });
+}
+
+async function greetWithDelay() {
+    let myGreeting = await getGreeting();
+    console.log(myGreeting);
+}
+greetWithDelay(); // Hi!
