856c32ea84c8b9847fa2221e5168b5cb9e00d22e
gnuffer <gfnuffer@gmail.com>
2021-11-08 16:12:25 +0100

    SPA: add fetching random Chuck Norris joke

--- spa/live-coding/08-fetch-1/index.js
+++ spa/live-coding/08-fetch-1/index.js
@@ -8,3 +8,14 @@ async function getPosts() {
     console.log('with async/await: ', dataJson);
 }
 getPosts();
+
+// fetch('https://api.chucknorris.io/jokes/random')
+//     .then(response => response.json())
+//     .then(data => console.log(data.value));
+
+async function getJoke() {
+    let response = await fetch('https://api.chucknorris.io/jokes/random');
+    let dataJson = await response.json();
+    console.log(dataJson.value);
+}
+getJoke();
