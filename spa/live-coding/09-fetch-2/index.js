3ad949b3c79effb0f7aed768fd14f2d36d654a8e
gnuffer <gfnuffer@gmail.com>
2021-11-11 19:43:51 +0100

    Add localStorage to fetch-post assignment

--- spa/live-coding/09-fetch-2/index.js
+++ spa/live-coding/09-fetch-2/index.js
@@ -4,6 +4,18 @@ const body = document.getElementById('body');
 const userId = document.getElementById('userId');
 const list = document.getElementById('list');
 
+// check if localStorage stores a value at key "textArr"; if so, parse this value 
+// as JSON and assign it to the textArr constant; else assign an empty array
+const textArr = JSON.parse(localStorage.getItem('textArr')) || [];
+
+// for each string (text) in textArr, create a list item with this string (text)
+// as textContent and append it to the list
+textArr.forEach(text => {
+    let listItem = document.createElement('li');
+    listItem.textContent = text;
+    list.appendChild(listItem);
+});
+
 const handleSubmit = function () {
     fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
@@ -16,12 +28,13 @@ const handleSubmit = function () {
             'Content-type': 'application/json; charset=UTF-8',
         },
     })
-    .then((response) => response.json())
-    .then((obj) => {
+    .then(response => response.json())
+    .then(obj => {
         let listItem = document.createElement('li');
-        listItem.textContent = `Title: ${obj.title}, body: ${obj.body}, userId: ${obj.userId}`;
-        // let textNode = document.createTextNode(text);
-        // listItem.appendChild(textNode);
+        let text = `Title: ${obj.title}, body: ${obj.body}, userId: ${obj.userId}`;
+        textArr.push(text);
+        listItem.textContent = text;
+        localStorage.setItem('textArr', JSON.stringify(textArr));
         list.appendChild(listItem);
     });
 };
