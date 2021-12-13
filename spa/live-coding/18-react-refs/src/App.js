3334dd91ac6ba48920de649b2ab2ea938682e84c
gnuffer <gfnuffer@gmail.com>
2021-12-13 20:23:32 +0100

    Add react refs

---
+++ spa/live-coding/18-react-refs/src/App.js
@@ -0,0 +1,20 @@
+import { useRef } from 'react';
+
+function App() {
+  const inputRef = useRef(null);
+
+  function handleClick() {
+    inputRef.current.focus();
+  }
+
+  return (
+    <>
+      <input ref={inputRef} />
+      <button onClick={handleClick}>
+        Focus the input
+      </button>
+    </>
+  );
+}
+
+export default App;
