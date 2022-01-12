746dec59385860c4d9630aa6d1b7bc5d199190f1
Mostafa Othman <mostafa.othman@digitalcareerinstitute.org>
2022-01-12 09:56:52 +0100

    star_task

--- backend/1-run_js_on_os/stars.js
+++ backend/1-run_js_on_os/stars.js
@@ -9,3 +9,50 @@
  *  *****
  *  *****
  */
+
+/*dfgsdfg
+adsfgadsfg
+adsfg*/
+
+/**
+ * this for args
+ */
+let args = process.argv.slice(2)
+/**
+ * this is for charactor
+ */
+let ch = args[0]
+let rows = args[1]
+// let rows = args[1]? args[1]: 5
+let columns = args[2]
+
+/**
+ * This Function Creates a stars
+ * @author Mostafa
+ * @param {boolean} character 
+ * @param {number} rows 
+ * @param {number} columns 
+ */
+function createStars(character="*", rows=2, columns=5){
+    let str = ""
+    for(let j=0; j< rows; j++){
+        for(let i=0; i<columns; i++){
+            str += character
+        }
+        str +="\n"
+    }
+    console.log(str)
+}
+
+createStars(ch, rows, columns)
+foo({})
+/**
+ * 
+ * @param {object} a 
+ * @returns 
+ */
+function foo(a={}){
+    return 0
+}
+
+console.log(args)
