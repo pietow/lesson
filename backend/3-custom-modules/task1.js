009d3ab072efc470a96605312ad9866923079519
ahmad-teaching <ahmad.osman@digitalcareerinstitute.org>
2022-01-13 14:30:25 +0100

    task start

---
+++ backend/3-custom-modules/task1.js
@@ -0,0 +1,17 @@
+// write a code to let user enter his username and password to register:
+// #what do you want to do:
+// #register
+// #enter your username:
+// #fbw
+// #enter your password:
+// 12345678
+
+// the program should save user data in a json file as array of objects
+// [
+//     {
+//         username: 'fbw',
+//         password: "[hashed password]"
+//     }
+// ]
+
+// #your registration is done
