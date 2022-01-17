d6db1c0cb920441862d1443bc064ff8633a752d0
Mostafa Othman <mostafa.othman@digitalcareerinstitute.org>
2022-01-17 12:03:48 +0100

    run_server

--- backend/4-http-server/createServer/app.js
+++ backend/4-http-server/createServer/app.js
@@ -1,6 +1,15 @@
 // to create server in nodejs use 'http'
 const http = require('http')
+const fs = require('fs')
 const server = http.createServer((req, res)=>{
+    // console.log(req.headers)
+    // console.log(req.url)
+    // store req.headers in file
+    fs.writeFile('req.json', JSON.stringify(req.headers), error=>{
+        if(error)throw error
+        console.log("header saved!")
+    })
+    res.end("Hello wolrd")
 
 })
 // console.log(http)
