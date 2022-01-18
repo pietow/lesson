9367b7a066445bc65a2ecdf971cb418d90184de2
Mostafa Othman <mostafa.othman@digitalcareerinstitute.org>
2022-01-18 13:19:09 +0100

    add_template

--- backend/4-http-server/get_method_project/app.js
+++ backend/4-http-server/get_method_project/app.js
@@ -3,27 +3,31 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
+// getting template
+let template = ``;
+fs.readFile("./index.html", (error, data) => {
+  if (error) throw error;
+  template = data.toString();
+});
+
 const server = http
   .createServer((req, res) => {
     // getting all products
     // for any request we send all products
     // console.log(url.parse(req.url, true));
-    const params = url.parse(req.url, true).query
+    const params = url.parse(req.url, true).query;
     if (params.id) {
       // asking for some product
-      fs.readFile("./products.json", (error, data)=>{
-          if(error){
-              res.writeHead(200, {"content-type": "text/html"}).end("<h1>Internal Server Error!</h1>")
-          }else{
-              const products  = JSON.parse(data.toString())
-              const product = products.find(item=>item.id == params.id)
-            //   console.log(product)
-            // getting template from index.html
-            fs.readFile("index.html", (err, html)=>{
-                if(err){
-                    res.writeHead(200, {"content-type": "text/html"}).end("<h1>Internal Server Error!</h1>")
-                }else{
-                    let content = `<div class="product"><h1>${product.brand}</h1>
+      fs.readFile("./products.json", (error, data) => {
+        if (error) {
+          res
+            .writeHead(200, { "content-type": "text/html" })
+            .end("<h1>Internal Server Error!</h1>");
+        } else {
+          const products = JSON.parse(data.toString());
+          const product = products.find((item) => item.id == params.id);
+          //   console.log(product)
+          let content = `<div class="product"><h1>${product.brand}</h1>
                             <ul>
                                 <li>model: ${product.model}</li>
                                 <li>power: ${product.power}</li>
@@ -33,20 +37,14 @@ const server = http
                                 <li>doors: ${product.doors}</li>
                             </ul></div>
                             <a href="/">Go To all products</a>
-                    `
-                    res.writeHead(200, {"content-type": "text/html"})
-                    res.end(html.toString().replace("@@", content).replace("[[", product.brand))
-                }
-            })
-            //   res.end(JSON.stringify(product))
-          }
-      })
-    }
-    else if(params.doors){
-        // return to client all cars whch have doors matching
-
-    }
-    else {
+                    `;
+          res.writeHead(200, { "content-type": "text/html" });
+          res.end(template.replace("@@", content).replace("[[", product.brand));
+        }
+      });
+    } else if (params.doors) {
+      // return to client all cars whch have doors matching
+    } else {
       fs.readFile("./products.json", (error, data) => {
         if (error) {
           res
@@ -62,18 +60,10 @@ const server = http
             container += `<div class="product"><h2>${product.brand}</h2> <p>Prise: ${product.prise}</p><a href="/?id=${product.id}">Read more</a></div>`;
           });
           container += `</div>`;
-          // console.log(container)
-          fs.readFile("./index.html", (err, html) => {
-            if (err) {
-              res
-                .writeHead(200, { "content-type": "text/html" })
-                .end("<h2>Internal server error</h2>");
-              console.log(err);
-            } else {
-              res.writeHead(200, { "content-type": "text/html" });
-              res.end(html.toString().replace("@@", container).replace("[[", "All Products"));
-            }
-          });
+          res.writeHead(200, { "content-type": "text/html" });
+          res.end(
+            template.replace("@@", container).replace("[[", "All Products")
+          );
         }
       });
     }
