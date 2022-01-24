38328de77074ae05fcd47b0db6b1874a84dbbca2
Mostafa Othman <mostafa.othman@digitalcareerinstitute.org>
2022-01-24 11:45:49 +0100

    add_task

--- backend/8-view-emgine/app.js
+++ backend/8-view-emgine/app.js
@@ -1,41 +1,54 @@
-const express = require('express')
-const logger = require('morgan')
+const express = require("express");
+const logger = require("morgan");
 
-const app = express()
+const app = express();
 // setting the port
-app.set('port', process.env.PORT || 3000)
+app.set("port", process.env.PORT || 3000);
 // setting a view engine ...
-app.set('view engine', 'ejs')
-app.set('views', __dirname + "/views")
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
-app.use(logger('dev'))
-
+app.use(logger("dev"));
 
 // getting main request
-app.get('/', (req, res)=>{
-    // res.sendFile(__dirname+'/index.html')
-    res.render('index',{title: "Home", content: "This is home page", dark:true})
-})
+app.get("/", (req, res) => {
+  // res.sendFile(__dirname+'/index.html')
+  let products = [
+    { name: "BMW", model: 2000, fuel: "Diesel" },
+    { name: "Ford", model: 2005, fuel: "Benzin" },
+    { name: "Opel", model: 2010, fuel: "Gas" },
+    { name: "Audi", model: 2020, fuel: "Diesel" },
+  ];
+  res.render("index", {
+    title: "Home",
+    content: "This is home page",
+    dark: true,
+    data: products
+  });
+});
 
-app.get('/about', (req, res)=>{
-    // res.sendFile(__dirname+'/about.html')
-    res.render('about', {title: "About", content: "This is About page"})
-})
+app.get("/about", (req, res) => {
+  // res.sendFile(__dirname+'/about.html')
+  res.render("about", { title: "About", content: "This is About page" });
+});
 
-app.get('/services', (req, res)=>{
-    // res.sendFile(__dirname+'/services.html')
-    res.render('services', {title: "Services", content: "This is Services page"})
-})
+app.get("/services", (req, res) => {
+  // res.sendFile(__dirname+'/services.html')
+  res.render("services", {
+    title: "Services",
+    content: "This is Services page",
+  });
+});
 
-app.get('/api', (req, res)=>{
-    res.json([{
-        name: "Mostafa", 
-        age: 30
-    }])
-})
+app.get("/api", (req, res) => {
+  res.json([
+    {
+      name: "Mostafa",
+      age: 30,
+    },
+  ]);
+});
 
-
-
-app.listen(app.get('port'),()=>{
-    console.log(`The server is running on Port: ${app.get('port')}`)
-})
+app.listen(app.get("port"), () => {
+  console.log(`The server is running on Port: ${app.get("port")}`);
+});
