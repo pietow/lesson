9732c530e52a13f371b6f0697bbf8fe1c6344ab0
Mostafa Othman <mostafa.othman@digitalcareerinstitute.org>
2022-01-24 15:16:55 +0100

    add_products_task

--- backend/8-view-emgine/app.js
+++ backend/8-view-emgine/app.js
@@ -7,29 +7,53 @@ app.set("port", process.env.PORT || 3000);
 // setting a view engine ...
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
-
+// Set public folder
+app.use('/public', express.static(__dirname + '/public'))
+// add bootstrab folder from node_modules
+app.use('/bootstrap', express.static(__dirname + "/node_modules/bootstrap"))
+app.use('/jquery', express.static(__dirname + "/node_modules/jquery"))
 app.use(logger("dev"));
 
 // getting main request
 app.get("/", (req, res) => {
+  //console.log(req.query)
   // res.sendFile(__dirname+'/index.html')
-  let products = [
-    { name: "BMW", model: 2000, fuel: "Diesel" },
-    { name: "Ford", model: 2005, fuel: "Benzin" },
-    { name: "Opel", model: 2010, fuel: "Gas" },
-    { name: "Audi", model: 2020, fuel: "Diesel" },
+  let products = [{
+      name: "BMW",
+      model: 2000,
+      fuel: "Diesel"
+    },
+    {
+      name: "Ford",
+      model: 2005,
+      fuel: "Benzin"
+    },
+    {
+      name: "Opel",
+      model: 2010,
+      fuel: "Gas"
+    },
+    {
+      name: "Audi",
+      model: 2020,
+      fuel: "Diesel"
+    },
   ];
   res.render("index", {
     title: "Home",
     content: "This is home page",
-    dark: true,
+    dark: req.query.dark==="true" ? true : false,
     data: products
   });
 });
 
 app.get("/about", (req, res) => {
   // res.sendFile(__dirname+'/about.html')
-  res.render("about", { title: "About", content: "This is About page" });
+  res.render("about", {
+    title: "About",
+    content: "This is About page",
+    dark: req.query.dark==="true" ? true : false,
+  });
 });
 
 app.get("/services", (req, res) => {
@@ -37,16 +61,24 @@ app.get("/services", (req, res) => {
   res.render("services", {
     title: "Services",
     content: "This is Services page",
+    dark: req.query.dark==="true" ? true : false,
+  });
+});
+
+app.get("/products", (req, res) => {
+  // res.sendFile(__dirname+'/services.html')
+  res.render("products", {
+    title: "Products",
+    content: "This is Products page",
+    dark: req.query.dark==="true" ? true : false,
   });
 });
 
 app.get("/api", (req, res) => {
-  res.json([
-    {
-      name: "Mostafa",
-      age: 30,
-    },
-  ]);
+  res.json([{
+    name: "Mostafa",
+    age: 30,
+  }, ]);
 });
 
 app.listen(app.get("port"), () => {
