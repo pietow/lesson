d81f028bace47e9fea3f935849783cce95a6ed2c
Mostafa Othman <mostafa.othman@digitalcareerinstitute.org>
2022-01-25 11:46:00 +0100

    add_ejs_task

---
+++ backend/9-ejs-task/app.js
@@ -0,0 +1,36 @@
+/**
+ * 1- the project should has:
+ *  a- views (folder)
+ *      1- partials (folder)
+ *          a- cssLinks.ejs
+ *          b- jsLinks.ejs
+ *          c- navbar.ejs
+ *          d- footer.ejs
+ *      2- content (folder)
+ *          e- home.ejs
+ *          f- about.ejs
+ *          g- products.ejs
+ *          h- product.ejs
+ *      3- mainTemplate.ejs
+ *  b- routes (folder)
+ *      1- home.js
+ *      2- products.js
+ *      3- about.js
+ *  c- public (folder)
+ *      1- css (folder) 
+ *          a- style.css
+ *      2- js (folder)
+ *          a- script.js
+ *      3- images (folder)
+ *  d- data (folder)
+ *      products.json
+ * 
+ * 2- create Server using express
+ * 3- set accessble public folder and include bootstrap
+ * 4- set the view engine ejs
+ * 
+ * 6- make the following routes
+ *      a- "/" home
+ *      b- "/about" about
+ *      c- "/products" products
+ */
