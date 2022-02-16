5197b5026881a641d6fad7424801a77781218bcf
ahmad-teaching <ahmad.osman@digitalcareerinstitute.org>
2022-02-16 13:23:24 +0100

    session task2 start

--- backend/14-session/models/general.js
+++ backend/14-session/models/general.js
@@ -5,9 +5,15 @@ const login = (req, res) => {
 const aboutUs = (req, res) => {
     res.send('about us')
 }
-
+const loginPost = (req, res) => {
+    // write the code to check if sent data [username, password]
+    // matches the following ['admin', 'admin']
+    // if it matches res.json('done');
+    // else res.json('error')
+}
 
 module.exports = {
     login,
-    aboutUs
+    aboutUs,
+    loginPost
 }
--- backend/14-session/routes/generalRouter.js
+++ backend/14-session/routes/generalRouter.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-const {login, aboutUs} = require('../models/general')
+const {login, aboutUs, loginPost} = require('../models/general')
 
 router.get('/', login);
 router.get('/login', login);
-router.get('/aboutus', aboutUs)
+router.get('/aboutus', aboutUs);
+router.post('/login', loginPost);
 
-module.exports = router
+module.exports = router;
