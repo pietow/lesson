97883aee8bb21f4a04a73588cbbf4494856a2d7c
Mostafa <mostafa.othman@digitalcareerinstitute.org>
2022-02-24 10:03:41 +0100

    students_schema

--- 16-mongoos-schema/students.js
+++ 16-mongoos-schema/students.js
@@ -8,7 +8,102 @@
  *      d- class_id: Number [less than 100] required
  *      e- skills: Array of Strings (NOT REQUIRED)
  *      f- address: Object
- *              1- Country String [5, 20] required
+ *              1- country String [5, 20] required
  *              2- city String [5, 20] required
  *              3- street String  [5, 20] NOT REQUIRED
  */
+const mongoose = require("mongoose");
+const { log } = require("console");
+mongoose.connect("mongodb://localhost:27017/test", (error) => {
+    if (error) throw error;
+    log("Connected To MongoDb");
+});
+
+const students_schema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            minlength: [5, "Length should be more than 5"],
+            maxlength: 20,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            minlength: 12,
+            maxlength: 50,
+            validate: {
+                validator: (em) => {
+                    const EmailRegEx =
+                        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+                    return EmailRegEx.test(em);
+                },
+                message: 'Not a valied Email'
+            },
+        },
+        matrik_id: {
+            type: String,
+            length: 6,
+            required: true,
+            validate: {
+                validator: mk => {
+                    const mkReg = /[A-Z]{2}\d{4}/
+                    return mkReg.test(mk)
+                },
+                message: 'Not a valid Matrik nummer'
+            }
+        },
+        class_id: {
+            type: Number,
+            required: true,
+            max: 100
+        },
+        skills: {
+            type: [{// for each item inside
+                type: String,
+                minlength: 2,
+                maxlength: 10
+            }],
+            validate: [
+                (arr)=>arr.length<=3,
+               `Should be less than 3 items`
+            ]
+        },
+        address: {
+            country: {
+                type: String,
+                required: true,
+                minlength: [5, "Length should be more than 5"],
+                maxlength: 20,
+            },
+            city: {
+                type: String,
+                minlength: [5, "Length should be more than 5"],
+                maxlength: 20
+            },
+            street: {
+                type: String,
+                minlength: [5, "Length should be more than 5"],
+                maxlength: 20
+            }
+        }
+    },
+    { collection: "students" }
+);
+const Students = mongoose.model('Students', students_schema)
+let student_1 = {
+    name: "Heiko",
+    email: "heiko@gmail.com",
+    matrik_id: "RB3456",
+    class_id: 2,
+    skills: ["Javascript", "C#", "CSS"],
+    address:{
+        country: "Germany",
+        city: "Hamburg",
+        street: "Main Street"
+    }
+}
+Students.create(student_1).then(result=>{
+    log("Success",result)
+}).catch(error=>log(error.message))
--- backend/5-express/README.md
+++ backend/5-express/README.md
@@ -2,4 +2,18 @@
 
     - express
     - routing
-    
+
+# Standard way to create Express Server
+
+### 1. In Project directory, open Terminal and hit this command:
+
+``` npm init ```
+
+> This will ask some quistions about the project, like package name, version, description ...<br>At the end, type yes to confirm.<br>A new file will be created which is ``` pakage.json ``` that will contains all information we entered.
+
+Short way also to start an express is to type in terminal:
+
+``` npm init -y ```
+
+> This will automaticly create ``` package.json ``` file with standard values, like the package name will be the folder name, version will be 1.0.0, and no description, and if there is a git repository on the folder will be added, and the entro file will be ``` index.js ```.
+
