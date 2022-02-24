deb624b633b490ecaee17ef4ec47b2bad5c01beb
Mostafa <mostafa.othman@digitalcareerinstitute.org>
2022-02-24 15:32:08 +0100

    Front_end_done

--- backend/17-schema-populate/routes/admin.js
+++ backend/17-schema-populate/routes/admin.js
@@ -7,6 +7,12 @@ router.get('/addauthor', (req, res)=>{
     res.render('addAuthor')
 })
 
+router.post('/addauthor', (req, res)=>{
+    // req.body
+    // store the author
+    res.json({error: "Mission failed successfuly"})
+})
+
 
 
 
